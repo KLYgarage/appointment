bb753be9f67aaad7f0757c315c187252ef748589
kenny <kennykarnama@gmail.com>
2018-07-20 12:20:02 +0700

    Refactor available function : change function param, re-add createRFC33899 function, change test case

--- src/Attendee.php
+++ src/Attendee.php
@@ -82,7 +82,7 @@ class Attendee
      * List events based on calendarID
      * @param  string $startTime string date('c') format
      * @param  string $endTime string date('c') format
-     * @return \Google_Service_Calendar_Event
+     * @return array
      * @throws \Google_Service_Exception
      */
     public function listEvents($calendarId, $options = array())
